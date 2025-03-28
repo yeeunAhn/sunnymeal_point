040c655df0e74e3803aab210f8c0b1e0514607dd
hurler <yeeun780@gmail.com>
2025-03-28 16:10:24 +0900

    1차

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,26 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase.js
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"; // Firestore import
+import { getAuth } from "firebase/auth"; // 인증을 사용하는 경우
+
+// Firebase 설정
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
-  measurementId: process.env.REACT_APP_measurementId,
+  apiKey: "AIzaSyAAfwZVf7T_PeqSd6F_LgU16FUSw0kiwjU",
+  authDomain: "sunnymealpoint.firebaseapp.com",
+  projectId: "sunnymealpoint",
+  storageBucket: "sunnymealpoint.appspot.com",
+  messagingSenderId: "1082060132449",
+  appId: "1:1082060132449:web:b64f18e4db6950014c1a20",
+  measurementId: "G-YB96N40QV6",
 };
 
-// Initialize Firebase
+// Firebase 앱 초기화
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Firestore와 인증 객체를 가져오기
+const db = getFirestore(app); // Firestore 객체 초기화
+const auth = getAuth(app); // 인증 객체 초기화 (선택 사항)
+
+// db와 auth 객체를 외부에서 사용할 수 있도록 export
+export { db, auth };
